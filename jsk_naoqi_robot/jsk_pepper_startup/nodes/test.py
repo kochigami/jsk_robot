6a58bf35617ac47900fa1d5aed332ca0eb44a47e
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2019-05-04 10:15:22 +0900

    update test files

--- jsk_naoqi_robot/jsk_pepper_startup/nodes/test.py
+++ jsk_naoqi_robot/jsk_pepper_startup/nodes/test.py
@@ -6,13 +6,18 @@ test
 '''
 
 import rospy
+from std_msgs.msg import String
 
 def test():
-    print "test"
+    default_ip = rospy.get_param("~ip", "127.0.0.1")
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    r = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        rospy.loginfo(default_ip)
+        pub.publish(str(default_ip))
+        r.sleep()
     
 if __name__=="__main__":
     rospy.init_node("test")
-    nao_ip = rospy.get_param('~nao_ip', "777")
-    print nao_ip
     test()
     rospy.spin()
