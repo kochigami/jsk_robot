209b5f30c8cf4f3af5e236cd6f039616dc66f077
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2019-05-18 09:54:05 +0900

    add speech detection

--- jsk_naoqi_robot/jsk_pepper_startup/nodes/start_speech_recognition.py
+++ jsk_naoqi_robot/jsk_pepper_startup/nodes/start_speech_recognition.py
@@ -6,39 +6,39 @@ from subprocess import *
 from speech_recognition_msgs.srv import (
     SpeechRecognition,
     SpeechRecognitionRequest)
+from std_msgs.msg import Bool
 
-def start_speech_recognition_service(req):
-    rospy.wait_for_service('speech_recognition')
-    speech_recognition_proxy = rospy.ServiceProxy('speech_recognition', SpeechRecognition)
-    res = speech_recognition_proxy(req)
-    return res
-
-def detect_calling():
-    req = SpeechRecognitionRequest()
-    robot_name = rospy.get_param("~robot_name", None)
-    req.vocabulary.words.append("pepper")
-    if robot_name:
-        req.vocabulary.words.append(robot_name)
-    req.duration = 10
-    req.threshold = 0.3
-    res = start_speech_recognition_service(req)
-    if res.result.transcript is not None:
-        return res.result.transcript
-    else:
-        return None
-    
-def start_speech_recognition():
-    # TODO: if some signal detected
-    if True:
-        ans = detect_calling()
-        print ans # []
-        if ans and len(ans) > 0:
-            print ans 
-            print "someone is calling"
-            # TODO: add more speech recognition
+class StartSpeechRecognition():
+    def __init__(self):
+        # sub
+        self.sub = rospy.Subscriber("speech_detection", Bool, self.detect_calling)
+        
+    def detect_calling(self, msg):
+        #self.speech_detected = msg.data
+        if msg.data:
+            req = SpeechRecognitionRequest()
+            req.vocabulary.words.append("おはよう")
+            req.vocabulary.words.append("おやすみ")
+            req.vocabulary.words.append("紹介して")
+            req.vocabulary.words.append("ハンコ押して")
+            req.vocabulary.words.append("配って")
+            req.vocabulary.words.append("代わって")
+            req.vocabulary.words.append("移動して")
+            req.duration = 10 # change to 5?
+            req.threshold = 0.3
+            res = start_speech_recognition_service(req)
+            if res.result.transcript is not None:
+                return res.result.transcript
+            else:
+                return None
+        
+    def start_speech_recognition_service(self, req):
+        rospy.wait_for_service('speech_recognition')
+        speech_recognition_proxy = rospy.ServiceProxy('speech_recognition', SpeechRecognition)
+        res = speech_recognition_proxy(req)
+        return res
         
 if __name__ == "__main__":
     rospy.init_node("start_speech_recognition")
-    start_speech_recognition()
-    #rospy.spin()
+    rospy.spin()
     exit(0)
