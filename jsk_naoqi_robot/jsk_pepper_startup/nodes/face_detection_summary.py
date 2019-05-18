008a263a6eab0a28c99d928d38a948f9f14999d3
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2019-05-19 00:08:11 +0900

    use connection based nodelet

--- jsk_naoqi_robot/jsk_pepper_startup/nodes/face_detection_summary.py
+++ jsk_naoqi_robot/jsk_pepper_startup/nodes/face_detection_summary.py
@@ -2,32 +2,40 @@
 # -*- coding: utf-8 -*-
 
 import rospy
+from jsk_topic_tools import ConnectionBasedTransport
 from opencv_apps.msg import FaceArrayStamped
 from std_msgs.msg import Bool
 
-class FaceDetectionSummary():
+class FaceDetectionSummary(ConnectionBasedTransport):
     def __init__(self):
-        # sub
-        self.sub = rospy.Subscriber("/face_detection/faces", FaceArrayStamped, self.detect_calling)
-        # pub
         self.pub = rospy.Publisher("/speech_recognition_start_signal", Bool, queue_size=1)
+
+        self.size = rospy.get_param("~size", 70)
+        self.count_limit = rospy.get_param("~count_limit", 10)
+        self.time_limit = rospy.get_param("~time_limit", 7)
 
         self.time = rospy.Time.now()
         self.count = 0
+
+    def subscribe(self):
+        self.sub = rospy.Subscriber("/face_detection/faces", FaceArrayStamped, self.detect_calling)
+
+    def unsubscribe(self):
+        self.sub.unregister()
 
     def detect_calling(self, msg):
         if len(msg.faces) > 0:
             faces = msg.faces
             face = faces[0]
-            if face.face.width > 70 and face.face.height > 70:
+            if face.face.width > self.size and face.face.height > self.size:
                 if self.count == 0:
                     self.time = rospy.Time.now()
                     self.count += 1
                 else:
                     now = rospy.Time.now()
-                    if self.count < 10 and (rospy.Time.now() - self.time).secs < 7:
+                    if self.count < self.count_limit and (rospy.Time.now() - self.time).secs < self.time_limit:
                         self.count += 1
-                    elif (self.count > 10 or self.count == 10) and (rospy.Time.now() - self.time).secs < 7:
+                    elif (self.count > self.count_limit or self.count == self.count_limit) and (rospy.Time.now() - self.time).secs < self.time_limit:
                         msg = Bool()
                         msg.data = True
                         self.pub.publish(msg)
